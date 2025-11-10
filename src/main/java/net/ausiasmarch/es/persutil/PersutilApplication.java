e1629688f07c30cd033ddf329f1324b9461ffb7a
NoSoyVerde <hectorkotor@gmail.com>
2025-11-10 12:06:45 +0100

    Primer commit

---
+++ src/main/java/net/ausiasmarch/es/persutil/PersutilApplication.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.es.persutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersutilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersutilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ausiasmarch/es/persutil/PersutilApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.es.persutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersutilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
