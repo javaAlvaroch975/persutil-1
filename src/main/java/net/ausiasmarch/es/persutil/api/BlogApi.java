8c9a3f0926aebcb9116e7843d31354ebdba0f30a
NoSoyVerde <hectorkotor@gmail.com>
2025-11-11 10:09:30 +0100

    Service creado

--- src/main/java/net/ausiasmarch/es/persutil/api/BlogApi.java
+++ src/main/java/net/ausiasmarch/es/persutil/api/BlogApi.java
@@ -1,18 +1,48 @@
 package net.ausiasmarch.es.persutil.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.ausiasmarch.es.persutil.service.AleatorioService;
 
 @RestController
 @RequestMapping("/blog")
 public class BlogApi {
 
+    @Autowired
+    AleatorioService aleatorioService;
+
     @GetMapping("/saludar")
     public ResponseEntity<String> saludar() {
         return new ResponseEntity<>("¡Hola desde la API de Blog!", HttpStatus.OK);
+
+    }
+
+    @GetMapping("/saludar/{nombre}")
+    public ResponseEntity<String> saludarNombre(@PathVariable String nombre) {
+        return new ResponseEntity<>("¡Hola " + nombre + " desde la API de Blog!", HttpStatus.OK);
+    }
+
+    @GetMapping("/saludar/buenosdias")
+    public ResponseEntity<String> saludarBuenosDias() {
+        return new ResponseEntity<>("¡Buenos días desde la API de Blog!", HttpStatus.OK);
+    }
+
+    @GetMapping("/aleatorio")
+    public ResponseEntity<Integer> aleatorio() {
+        int numeroAleatorio = (int) (Math.random() * 100) + 1;
+        return new ResponseEntity<>(numeroAleatorio, HttpStatus.OK);
+    }
+
+    @GetMapping("/aleatorio/service/{min}/{max}")
+    public ResponseEntity<Integer> aleatorioEnRango(@PathVariable int min, @PathVariable int max) {
+        int numeroAleatorio = aleatorioService.generarNumeroAleatorio(min, max);
+        return new ResponseEntity<>(numeroAleatorio, HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/net/ausiasmarch/es/persutil/entity/BlogEntity.java
@@ -0,0 +1,33 @@
+package net.ausiasmarch.es.persutil.entity;
+
+public class BlogEntity {
+    private Long id;
+    private String titulo;
+    private String contenido;
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+}
+    
